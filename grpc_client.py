087b52fae41336ca5f8ca78c7e06318ee77dc9af
Irvin Lim <irvinlimweiquan@gmail.com>
2020-02-25 21:09:32 -0500

    Fix client

--- grpc_client.py
+++ grpc_client.py
@@ -1,3 +1,5 @@
+import sys
+
 import grpc
 
 # import the generated classes
@@ -5,20 +7,20 @@ import pb.demo_pb2
 import pb.demo_pb2_grpc
 
 # open a gRPC channel
-channel = grpc.insecure_channel('localhost:50051')
+channel = grpc.insecure_channel('localhost:8888')
 
 # create a stub (client)
 stub_cart = pb.demo_pb2_grpc.CartServiceStub(channel)
-stub_ad = pb.demo_pb2_grpc.AdServiceStub(channel)
-stub_rec = pb.demo_pb2_grpc.RecommendationServiceStub(channel)
+# stub_ad = pb.demo_pb2_grpc.AdServiceStub(channel)
+# stub_rec = pb.demo_pb2_grpc.RecommendationServiceStub(channel)
 
 # create a valid request message
-cart_request = pb.demo_pb2.GetCartRequest(1)
-response_cart = stub_card.GetCart(cart_request)
-# print(response.value)
+cart_request = pb.demo_pb2.GetCartRequest(user_id=sys.argv[1])
+response_cart = stub_cart.GetCart(cart_request)
+print(response_cart)
 
-ad_request = pb.demo_pb2.AdRequest()
-response_ad = stub_ad.GetAds(ad_request)
+# ad_request = pb.demo_pb2.AdRequest()
+# response_ad = stub_ad.GetAds(ad_request)
 
-rec_request = pb.demo_pb2.ListRecommendationsRequest()
-response_rec = stub_rec.ListRecommendations(rec_request)
+# rec_request = pb.demo_pb2.ListRecommendationsRequest()
+# response_rec = stub_rec.ListRecommendations(rec_request)
