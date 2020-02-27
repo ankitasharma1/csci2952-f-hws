aec78c446c1cddb5b2c30fff4fe147f4ed871404
Irvin Lim <irvinlimweiquan@gmail.com>
2020-02-27 09:05:22 -0500

    Change OPA image to irvinlim/opa-ambassador-plugin, add OPA policy, update gRPC client

--- grpc_client.py
+++ grpc_client.py
@@ -1,34 +1,60 @@
 import sys
+
+import click
 import grpc
 
 # import the generated classes
 import pb.demo_pb2
 import pb.demo_pb2_grpc
 
-NUM_PARAMS = 2
+DEFAULT_ADDR = 'localhost:8888'
+config = {'addr': DEFAULT_ADDR}
 
-if len(sys.argv) >= NUM_PARAMS:
-    service = sys.argv[1]
 
+@click.group()
+@click.option('--addr', default=DEFAULT_ADDR, help='set the address of the API gateway')
+def cli(addr):
+    if addr:
+        config['addr'] = addr
+
+
+@cli.command()
+@click.argument('user_id')
+@click.option('--token', help='authorization token to pass to gRPC server')
+def get_cart(user_id, token):
     # open a gRPC channel
-    channel = grpc.insecure_channel('localhost:8888')
+    with grpc.insecure_channel(config['addr']) as channel:
+        # create a stub (client)
+        stub = pb.demo_pb2_grpc.CartServiceStub(channel)
 
-    if service == "cart":
-        param = sys.argv[2]
-        stub_cart = pb.demo_pb2_grpc.CartServiceStub(channel)
-        cart_request = pb.demo_pb2.GetCartRequest(user_id=param)
-        response_cart = stub_cart.GetCart(cart_request)
-        print(response_cart)
-    elif service == "ad":
-        stub_ad = pb.demo_pb2_grpc.AdServiceStub(channel)
-        ad_request = pb.demo_pb2.AdRequest()
-        response_ad = stub_ad.GetAds(ad_request)
-        print(response_ad)
-    elif service == "rec":
-        param = sys.argv[2]        
-        stub_rec = pb.demo_pb2_grpc.RecommendationServiceStub(channel)
-        rec_request = pb.demo_pb2.ListRecommendationsRequest(user_id=param, product_ids=[])
-        response_rec = stub_rec.ListRecommendations(rec_request)
-        print(response_rec)
-else:
-    print("Require [service] [param]")
+        # create a valid request message
+        request = pb.demo_pb2.GetCartRequest(user_id=user_id)
+
+        try:
+            # Pass metadata and call method
+            response, call = stub.GetCart.with_call(request, metadata=get_metadata(token))
+            click.secho('Response received:', fg='green')
+            print(response)
+        except grpc.RpcError as rpc_error:
+            # Handle error
+            click.secho('RPC failed:', fg='red')
+            print(rpc_error)
+            sys.exit(1)
+
+
+def get_metadata(token):
+    if token:
+        return (('authorization', 'Bearer ' + token), )
+
+    return tuple()
+
+
+if __name__ == "__main__":
+    cli()
+
+# TODO
+# stub_ad = pb.demo_pb2_grpc.AdServiceStub(channel)
+# stub_rec = pb.demo_pb2_grpc.RecommendationServiceStub(channel)
+
+# ad_request = pb.demo_pb2.AdRequest()
+# response_ad = stub_ad.GetAds(ad_request)
